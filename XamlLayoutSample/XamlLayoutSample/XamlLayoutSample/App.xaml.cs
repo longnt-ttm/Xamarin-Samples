d9b60e39380d30da1b17ab03dbd19c0d897b79fc
longnt-ttm <30993696+longnt-ttm@users.noreply.github.com>
2017-10-25 16:53:53 +0700

    First sample about layout

---
+++ XamlLayoutSample/XamlLayoutSample/XamlLayoutSample/App.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamlLayoutSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new XamlLayoutSample.MainPage();
+            MainPage = new TabbedPage
+            {
+                Children =
+                {
+                    new Page1
+                    {
+                        Title = "Page 1"
+                    },
+                    new Page2
+                    {
+                        Title = "Page 2"
+                    },
+                    new Page3
+                    {
+                        Title = "Page 3"
+                    },
+                    new Page4
+                    {
+                        Title = "Page 4"
+                    },
+                    new Page5
+                    {
+                        Title = "Page 5"
+                    },
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
